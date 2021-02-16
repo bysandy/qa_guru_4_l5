e5a99be990b75491014dff01f4662e5ec42ab832
Bysandy <leeonid@gmil.com>
2021-02-16 08:07:42 +0300

    Added Listeners

--- src/test/java/io/qaguru/eroshenkoam/GitHubIssueTest.java
+++ src/test/java/io/qaguru/eroshenkoam/GitHubIssueTest.java
@@ -1,6 +1,8 @@
 package io.qaguru.eroshenkoam;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -9,14 +11,15 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class GitHubIssueTest extends TestBase {
-
+    private static final String BASE_URL = "https://github.com";
     private static final String REPOSITORY = "bysandy/qa_guru_4_l5";
     private static final String ISSUE_NUMBER = "#3";
 
     @Test
     public void testIssueSearch(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
         // Open github.com
-        open("https://github.com");
+        open(BASE_URL);
         // Fill in the search field  bysandy/qa_guru_4_l5
         // Tap on the Search button
         $(".header-search-input").click();
